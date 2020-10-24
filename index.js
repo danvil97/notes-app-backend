3f20b025a6e69e843f087d5b0372aa12f30c7a27
Danius Vilimas <danius_vilimas@epam.com>
2020-10-25 01:14:18 +0300

    remove swagger

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
+const port = "3001";
 const express = require("express");
 const morgan = require("morgan");
-
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
-
-const port = "3001";
 
 const app = express();
 
@@ -12,27 +8,6 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require("./routes/index.routes"));
-
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "note-api Express API with Swagger",
-      version: "0.1.0",
-      description:
-        "This is a simple CRUD API application made with Express and documented with Swagger",
-    },
-    servers: [
-      {
-        url: "http://localhost:3001/api/v1/notes",
-      },
-    ],
-  },
-  apis: ["./routes/note.routes.js"],
-};
-
-const specs = swaggerJsdoc(options);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello world" });
